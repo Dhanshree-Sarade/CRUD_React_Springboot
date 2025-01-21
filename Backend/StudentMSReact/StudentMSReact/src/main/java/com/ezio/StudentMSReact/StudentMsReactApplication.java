42c15961aa5aa82e50ab450d7f875974aaa5ec53
unknown <Dhanshree-Sarade>
2025-01-21 15:09:19 +0530

    Added frontend and backend as submodules

---
+++ Backend/StudentMSReact/StudentMSReact/src/main/java/com/ezio/StudentMSReact/StudentMsReactApplication.java
@@ -0,0 +1,13 @@
+package com.ezio.StudentMSReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMsReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMsReactApplication.class, args);
+	}
+
+}
---
+++ Backend/StudentMSReact/StudentMSReact/src/test/java/com/ezio/StudentMSReact/StudentMsReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezio.StudentMSReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMsReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
